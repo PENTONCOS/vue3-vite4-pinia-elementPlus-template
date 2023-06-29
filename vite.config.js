d3e6907c0b02f1d9c5608efccd2d3249b51ba6b3
jiapandong <jiapandong@supconit.com>
2023-06-29 08:57:52 +0800

    test: 测试base

--- vite.config.js
+++ vite.config.js
@@ -69,7 +69,7 @@ const baseConfig = {
 };
 
 export default defineConfig((command, mode) => {
-  console.log('command', command)
+  console.log('command', command, loadEnv(mode, process.cwd().VITE_BASE_HistoryBaseURL))
   if (command === 'build') {
     return {
       ...baseConfig,
