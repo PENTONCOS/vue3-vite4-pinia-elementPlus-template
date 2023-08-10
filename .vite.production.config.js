61c545d782f51933f63ca4fadccb045d8d0eabf0
jiapandong <jiapandong@supconit.com>
2023-08-10 10:09:57 +0800

    feat: 压缩第三方包优化

--- .vite.production.config.js
+++ .vite.production.config.js
@@ -3,7 +3,14 @@ import viteBaseConfig from './.vite.base.config.js'
 export default {
   ...viteBaseConfig,
   build: {
-    assetsInlineLimit: 4096 // 图片转 base64 编码的阈值
+    assetsInlineLimit: 4096, // 图片转 base64 编码的阈值
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+        },
+      },
+    },
   },
 
   esbuild: {
