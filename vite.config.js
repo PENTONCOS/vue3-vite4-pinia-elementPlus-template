345ae859fc67dc8fb41654e877e2cc3f2839f247
jiapandong <jiapandong@supconit.com>
2023-10-11 09:52:37 +0800

    refactor: 优化环境变量获取逻辑

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import viteDevelopmentConfig from './.vite.development.config.js';
 import viteProductionConfig from './.vite.production.config.js';
 import viteTestConfig from './.vite.test.config.js';
 
-export default defineConfig(({ command, mode }) => {
-  let config = {
-    base: loadEnv(mode, process.cwd()).VITE_BASE_HistoryBaseURL,
-  };
+const strategies = {
+  development: viteDevelopmentConfig,
+  production: viteProductionConfig,
+  test: viteTestConfig,
+};
 
+export default defineConfig(({ command, mode }) => {
   return {
-    ...config,
-    ...(mode === 'development' ? viteDevelopmentConfig : null),
-    ...(mode === 'production' ? viteProductionConfig : null),
-    ...(mode === 'test' ? viteTestConfig : null),
+    base: loadEnv(mode, process.cwd()).VITE_BASE_HistoryBaseURL,
+    ...strategies[mode],
   };
 });
